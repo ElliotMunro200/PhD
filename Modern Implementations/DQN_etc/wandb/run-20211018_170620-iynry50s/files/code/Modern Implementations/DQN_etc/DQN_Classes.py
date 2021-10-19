acd0d2a2af86fc78550ace47c21833d29d3db91c
ElliotMunro200 <emunro2020@gmail.com>
2021-10-20 10:24:47 +1300

    further coding progression

--- Modern Implementations/DQN_etc/wandb/run-20211018_170620-iynry50s/files/code/Modern Implementations/DQN_etc/DQN_Classes.py
+++ Modern Implementations/DQN_etc/wandb/run-20211018_170620-iynry50s/files/code/Modern Implementations/DQN_etc/DQN_Classes.py
@@ -231,8 +231,8 @@ if __name__ == "__main__":
     #DQN
     if DQN:
         env = gym.make(config["env_name"]) #SDP_env()
-        num_states = env.observation_space.shape[0] #env.num_states
-        num_actions = env.action_space.n #env.num_actions
+        num_states = env.num_states
+        num_actions = env.num_actions
         model = DQNet(num_states, num_actions)
         optimizer = optim.Adam(model.parameters())
         replay_buffer = ReplayBuffer(buffer_size)
